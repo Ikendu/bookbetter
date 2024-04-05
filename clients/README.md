4d1f7d69f55c14e67e23807255d954c40bd1eb69
Aniede Chibundu <ndubest56@gmail.com>
2024-04-05 02:49:11 +0100

    search section added

---
+++ clients/README.md
@@ -0,0 +1,9 @@
+# Book Better
+
+A booking app for vacation destinations and room for rented
+A full stack booking app that wooks like the airbnb website
+Done for learning and practical purpurses
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+  "# bookbetter"
---
+++ clients/src/App.jsx
@@ -0,0 +1,40 @@
+import "./App.css";
+import HomeIcon from "./assets/Icons/HomeIcon";
+import MobileViewIcon from "./assets/Icons/MobileViewIcon";
+import SearchIcon from "./assets/Icons/SearchIcon";
+import UserIcon from "./assets/Icons/UserIcon";
+
+function App() {
+  return (
+    <div>
+      <header className="p-4 flex justify-between">
+        <div className="flex gap-1 items-center">
+          <div>
+            <HomeIcon />
+          </div>
+          <span className="font-bold text-lg">lifella</span>
+        </div>
+        <div className="flex gap-2 border border-pink-900 rounded-full py-2 px-4 shadow-md shadow-pink-400">
+          <div>Anywhere</div>
+          <div className="border-l border-pink-900"></div>
+          <div>Any week</div>
+          <div className="border-l border-pink-900"></div>
+          <div>Add guests</div>
+          <button className="bg-primary text-white rounded-full p-1">
+            <SearchIcon />
+          </button>
+        </div>
+        <div className="flex gap-2 border border-pink-900 rounded-full py-2 px-4 shadow shadow-pink-400">
+          <div>
+            <MobileViewIcon />
+          </div>
+          <div className="bg-primary rounded-full text-white">
+            <UserIcon />
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
