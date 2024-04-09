a377cbdd7b1f48dd8547f466f7c2c36a2beed918
Aniede Chibundu <ndubest56@gmail.com>
2024-04-09 18:16:10 +0100

    some upate on route

--- clients/src/App.jsx
+++ clients/src/App.jsx
@@ -1,38 +1,13 @@
 import "./App.css";
-import HomeIcon from "./assets/Icons/HomeIcon";
-import MobileViewIcon from "./assets/Icons/MobileViewIcon";
-import SearchIcon from "./assets/Icons/SearchIcon";
-import UserIcon from "./assets/Icons/UserIcon";
 
 function App() {
+  // <Routes>
+  //   <Route index={ } />
+  // </Routes>;
   return (
     <div>
-      <header className="p-4 flex justify-between">
-        <div className="flex gap-1 items-center">
-          <div>
-            <HomeIcon />
-          </div>
-          <span className="font-bold text-lg">lifella</span>
-        </div>
-        <div className="flex gap-2 items-center border border-pink-900 rounded-full py-2 px-4 shadow-md shadow-pink-400">
-          <div>Anywhere</div>
-          <div className="border-l border-pink-900"></div>
-          <div>Any week</div>
-          <div className="border-l border-pink-900"></div>
-          <div>Add guests</div>
-          <button className="bg-primary text-white rounded-full p-1">
-            <SearchIcon />
-          </button>
-        </div>
-        <div className="flex gap-2 items-center border border-pink-900 rounded-full py-2 px-4">
-          <div>
-            <MobileViewIcon />
-          </div>
-          <div className="bg-primary rounded-full text-white border-2 border-pink-900 overflow-hidden">
-            <UserIcon />
-          </div>
-        </div>
-      </header>
+      Hello
+      
     </div>
   );
 }
---
+++ clients/src/Pages/IndexPage.jsx
@@ -0,0 +1,38 @@
+import HomeIcon from "../assets/Icons/HomeIcon";
+import MobileViewIcon from "../assets/Icons/MobileViewIcon";
+import SearchIcon from "../assets/Icons/SearchIcon";
+import UserIcon from "../assets/Icons/UserIcon";
+
+const IndexPage = () => {
+  return (
+    <div>
+      <header className="p-4 flex justify-between">
+        <div className="flex gap-1 items-center">
+          <div>
+            <HomeIcon />
+          </div>
+          <span className="font-bold text-lg">lifella</span>
+        </div>
+        <div className="flex gap-2 items-center border border-pink-900 rounded-full py-2 px-4 shadow-md shadow-pink-400">
+          <div>Anywhere</div>
+          <div className="border-l border-pink-900"></div>
+          <div>Any week</div>
+          <div className="border-l border-pink-900"></div>
+          <div>Add guests</div>
+          <button className="bg-primary text-white rounded-full p-1">
+            <SearchIcon />
+          </button>
+        </div>
+        <div className="flex gap-2 items-center border border-pink-900 rounded-full py-2 px-4">
+          <div>
+            <MobileViewIcon />
+          </div>
+          <div className="bg-primary rounded-full text-white border-2 border-pink-900 overflow-hidden">
+            <UserIcon />
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+export default IndexPage;
