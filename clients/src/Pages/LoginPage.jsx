5bb73e55c334ee7448d80f4ec97eaefd11cd931f
Aniede Chibundu <ndubest56@gmail.com>
2024-04-10 03:18:26 +0100

    login page component

--- clients/src/Pages/LoginPage.jsx
+++ clients/src/Pages/LoginPage.jsx
@@ -1,4 +1,13 @@
 const LoginPage = () => {
-  return <div>LoginPage should come here</div>;
+  return (
+    <div>
+      <h1 className="text-center text-2xl">Login</h1>
+      <form className=" max-w-2xl m-auto ">
+        <input type="text" placeholder="youremail@mail.com" />
+        <input type="password" placeholder="password" />
+        <button className="primary">Submit</button>
+      </form>
+    </div>
+  );
 };
 export default LoginPage;
