f9805c4e7f25452b72743e5519aaf268a5279e94
Aniede Chibundu <ndubest56@gmail.com>
2024-04-10 03:31:00 +0100

    center items between

--- clients/src/Pages/LoginPage.jsx
+++ clients/src/Pages/LoginPage.jsx
@@ -1,12 +1,14 @@
 const LoginPage = () => {
   return (
-    <div>
-      <h1 className="text-center text-2xl">Login</h1>
-      <form className=" max-w-2xl m-auto ">
-        <input type="text" placeholder="youremail@mail.com" />
-        <input type="password" placeholder="password" />
-        <button className="primary">Submit</button>
-      </form>
+    <div className="flex grow items-center  min-h-screen justify-around">
+      <div>
+        <h1 className="text-center text-2xl">Login</h1>
+        <form className=" max-w-2xl m-auto ">
+          <input type="text" placeholder="youremail@mail.com" />
+          <input type="password" placeholder="password" />
+          <button className="primary">Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
