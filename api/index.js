31d36b3a9c8546cf8a4f3ba858b5ff2efcaba8d3
Aniede Chibundu <ndubest56@gmail.com>
2024-04-11 07:21:20 +0100

    register end point

--- api/index.js
+++ api/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(express.json());
+
 app.get("/test", (req, res) => {
   res.json("test okay");
+});
+app.post("/register", (req, res) => {
+  const details = req.body;
+  res.json(details);
 });
 
 app.listen(4000);
