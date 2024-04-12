8ca4541bfac25f0040811a3fb369693e59f42cf3
Aniede Chibundu <ndubest56@gmail.com>
2024-04-12 23:59:18 +0100

    bcrypt for password encryption

--- api/index.js
+++ api/index.js
@@ -3,11 +3,14 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const UserModel = require("./models/User");
 const dotenv = require("dotenv").config();
+const bcrypt = require("bcryptjs");
 
 const app = express();
 
 app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use(express.json());
+
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 // mongodb+srv://ndubest56:11111234Aa@booking1.qjb5s42.mongodb.net/?retryWrites=true&w=majority&appName=booking1
 
@@ -19,7 +22,10 @@ app.get("/test", (req, res) => {
 
 app.post("/register", (req, res) => {
   const details = req.body;
-  UserModel.create(details);
+  UserModel.create({
+    ...details,
+    password: bcrypt.hashSync(password, bcryptSalt),
+  });
   res.json(details);
 });
 
