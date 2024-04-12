19d0df54839f34c8c4ee0ce09dcd92f0363cfaef
Aniede Chibundu <ndubest56@gmail.com>
2024-04-12 01:49:31 +0100

    precess dotenv config

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
 const app = express();
 
@@ -8,6 +10,8 @@ app.use(express.json());
 
 // mongodb+srv://ndubest56:11111234Aa@booking1.qjb5s42.mongodb.net/?retryWrites=true&w=majority&appName=booking1
 
+mongoose, connect(process.env.MONGO_URL);
+s
 app.get("/test", (req, res) => {
   res.json("test okay");
 });
