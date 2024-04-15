d6d313a2172d440c8b68b79aaf51eb7754da88c3
Aniede Chibundu <ndubest56@gmail.com>
2024-04-15 07:15:36 +0100

    add new place places

--- clients/src/Pages/PlacesPage.jsx
+++ clients/src/Pages/PlacesPage.jsx
@@ -3,11 +3,13 @@ import PlusSignIcon from "../assets/Icons/PlusSignIcon";
 
 export const PlacesPage = () => {
   return (
-    <div>
-      <Link>
-        <PlusSignIcon />
-        Add new
-      </Link>
+    <div className="m-6">
+      <div className="text-center">
+        <Link className="inline-flex gap-2 items-center bg-primary py-2 px-6 rounded-full cursor-pointer text-white  ">
+          <PlusSignIcon />
+          Add new
+        </Link>
+      </div>
     </div>
   );
 };
