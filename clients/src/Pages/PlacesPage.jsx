fde98e481c893a84eb7fcf30e06c428bdb1c9e1b
Aniede Chibundu <ndubest56@gmail.com>
2024-04-16 19:28:19 +0100

    new place

--- clients/src/Pages/PlacesPage.jsx
+++ clients/src/Pages/PlacesPage.jsx
@@ -6,15 +6,29 @@ export const PlacesPage = () => {
   console.log(action);
   return (
     <div className="m-6">
-      <div className="text-center">
-        <Link
-          to={"/account/places/new"}
-          className="inline-flex gap-2 items-center bg-primary py-2 px-6 rounded-full cursor-pointer text-white  "
-        >
-          <PlusSignIcon />
-          Add new
-        </Link>
-      </div>
+      {action !== "new" && (
+        <div className="text-center">
+          <Link
+            to={"/account/places/new"}
+            className="inline-flex gap-2 items-center bg-primary py-2 px-6 rounded-full cursor-pointer text-white  "
+          >
+            <PlusSignIcon />
+            Add new
+          </Link>
+        </div>
+      )}
+      {action === "new" && (
+        <div>
+          <form>
+            <input
+              type="text"
+              placeholder="title: for example, my lovely home"
+            />
+            <input type="text" placeholder="your address"/>
+            <input />
+          </form>
+        </div>
+      )}
     </div>
   );
 };
